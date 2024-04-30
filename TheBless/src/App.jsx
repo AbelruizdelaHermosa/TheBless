2a48a5298306543689ce380250f0b54624781246
Abel Ruiz de la Hermosa <abelruiz855@gmail.com>
2024-04-30 15:26:13 +0200

    nav

--- TheBless/src/App.jsx
+++ TheBless/src/App.jsx
@@ -1,31 +1,48 @@
-import { useState } from 'react'
-import bannerThebless from '../public/CartelTHEBLESS.png'
-import './App.css'
+import { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom'; // Cambio en la importación de Link
+import bannerThebless from '../public/CartelTHEBLESS.png';
+import logoTheBless from '../public/logo.jpg';
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const [isSticky, setSticky] = useState(false);
+
+  //navegador pegajoso
+  const stickyNav = () => {
+    window.scrollY > 150 ? setSticky(true) : setSticky(false);
+  };
+
+  useEffect(() => {
+    window.addEventListener('scroll', stickyNav);
+    return () => {
+      window.removeEventListener('scroll', stickyNav);
+    };
+  }, [])
+
+  const stickyClass = isSticky ? 'sticky-nav': '';
+
 
   return (
-    <>
-      <div>
-      <h1>THEBLESS</h1>
-        <a href="https://instagram.com/bythebless" target="_blank" >
-          <img src={bannerThebless} alt="logo" width="80%" height="50%"/>
-        </a>
-      </div>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click THEBLESS logos to learn more
-      </p>
-    </>
+  <div className={`${stickyClass} flex h-20 w-full items-center justify-between rounded-xl bg-white px-6 shadow-sm dark:bg-gray-600`}>
+      <Link className="flex items-center gap-2" to="/">
+            <img src={logoTheBless} alt="logo" className="w-10 rounded-2xl" />
+      </Link>
+        <nav className="flex items-center gap-6">
+          <Link className="text-sm font-medium hover:underline underline-offset-4 text-white" to="/">
+            Home
+          </Link>
+          <Link className="text-sm font-medium hover:underline underline-offset-4 text-white" to="/">
+            About
+          </Link>
+          <Link className="text-sm font-medium hover:underline underline-offset-4 text-white" to="/">
+            Services
+          </Link>
+          <Link className="text-sm font-medium hover:underline underline-offset-4 text-white" to="/">
+            Contact
+          </Link>
+        </nav>
+  </div>
   )
-}
+};
 
-export default App
+export default App;
