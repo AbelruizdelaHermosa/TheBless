a33dbfebc0793a832b99e1210aa8bc0660dde8b6
Abel Ruiz de la Hermosa <abelruiz855@gmail.com>
2024-04-30 10:28:25 +0200

    arranque aplicacion 2

--- TheBless/src/App.jsx
+++ TheBless/src/App.jsx
@@ -1,6 +1,5 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import bannerThebless from '../public/CartelTHEBLESS.png'
 import './App.css'
 
 function App() {
@@ -9,14 +8,11 @@ function App() {
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
+      <h1>THEBLESS</h1>
+        <a href="https://instagram.com/bythebless" target="_blank" >
+          <img src={bannerThebless} alt="logo" width="80%" height="50%"/>
         </a>
       </div>
-      <h1>Vite + React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
@@ -26,7 +22,7 @@ function App() {
         </p>
       </div>
       <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+        Click THEBLESS logos to learn more
       </p>
     </>
   )
