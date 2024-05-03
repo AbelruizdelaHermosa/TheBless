d3847a8600221488da9e972afa0e103c846e671c
Abel Ruiz de la Hermosa <abelruiz855@gmail.com>
2024-05-03 14:14:26 +0200

    cambios en diseño

--- TheBless/src/App.jsx
+++ TheBless/src/App.jsx
@@ -34,33 +34,30 @@ function App() {
 
   return (
     <div>
-      <nav className={`navbar ${stickyClass} flex h-20 w-11/12 items-center justify-between mx-auto rounded-xl mt-4 bg-white px-10 mb-10 shadow-sm dark:bg-gray-600`}>
-          <Link className="flex items-center gap-2" to="/">
-                <img src={logoTheBless} alt="logo" className="w-40 rounded-2xl" />
+      <nav className={`navbar ${stickyClass} flex h-20 md:h-24 lg:h-20 w-full md:w-11/12 items-center justify-between mx-auto rounded-xl mt-4 bg-white px-5 md:px-10 mb-10 shadow-sm dark:bg-gray-600`}>
+        <Link className="flex items-center gap-2" to="/">
+          <img src={logoTheBless} alt="logo" className="w-32 md:w-40 lg:w-44 rounded-2xl" />
+        </Link>
+        <div className="flex items-center gap-5 sm:gap-2 md:gap-10">
+          <Link className="text-lg font-medium hover:underline underline-offset-4 text-white" to="/">
+            Home
           </Link>
-            <div className="flex items-center gap-10">
-            <Link className="text-lg font-medium hover:underline underline-offset-4 text-white" to="/">
-                Home
-              </Link>
-              <Link className="text-lg font-medium hover:underline underline-offset-4 text-white" to="/">
-                About
-              </Link>
-              <Link className="text-lg font-medium hover:underline underline-offset-4 text-white" to="/">
-                Services
-              </Link>
-              <Link className="text-lg font-medium hover:underline underline-offset-4 text-white" to="/">
-                Contact
-              </Link>
-            </div>
+          <Link className="text-lg font-medium hover:underline underline-offset-4 text-white" to="/">
+            Contact
+          </Link>
+        </div>
       </nav>
 
-      <div className="relative mx-auto w-4/5 overflow-hidden h-screen">
+
+      <div className="relative mx-auto w-4/5 overflow-hidden h-screen
+      ">
         <ImageCarousel images={images}/>
       </div>
 
-      <hr className="my-6 w-5/6 border-gray-700 sm:mx-auto dark:border-gray-700 lg:my-8" />
+      <hr className="my-6 w-full md:w-5/6 border-gray-700 sm:mx-auto dark:border-gray-700 lg:mt-32" />
 
-      <aside className="sm:w-3/4 mx-auto pb-1">
+
+      <aside className="sm:w-3/4 mx-auto pt-10">
         <h1 id="titulo" className="text-center text-6xl p-8">STAGE 1</h1>
         <h1 id="titulo" className="text-center text-4xl">MONEY GOD TEE</h1>
         <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-20 pt-10 pb-10 m-8">
@@ -85,7 +82,7 @@ function App() {
         </div>
       </aside>
       
-      <hr className="my-6 pb-20 w-5/6 border-gray-700 sm:mx-auto dark:border-gray-700 lg:my-8" />
+      <hr className= "p-20 my-6 w-full md:w-5/6 border-gray-700 sm:mx-auto dark:border-gray-700 lg:my-8" />
 
       <div>
         <Footer />
