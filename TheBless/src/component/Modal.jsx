82c363b973d5ea3d33fc8970fa150d31b70ad768
Abel Ruiz de la Hermosa <abelruiz855@gmail.com>
2024-05-06 17:46:30 +0200

    cambio modal

--- TheBless/src/component/Modal.jsx
+++ TheBless/src/component/Modal.jsx
@@ -2,38 +2,32 @@ import React, { useState, useEffect, useRef } from 'react';
 import stage1 from '../../public/diseño.png';
 
 function Modal({ isOpen, onClose }) {
-  const modalRef = useRef(null);
+    const modalRef = useRef(null);
 
-  useEffect(() => {
-    if (isOpen) {
-      modalRef.current.focus();
-    }
-  }, [isOpen]);
-
-  const handleKeyDown = (event) => {
-    if (event.key === 'Escape') {
-      onClose();
-    }
-  };
-
-  if (!isOpen) return null;
+    useEffect(() => {
+      if (isOpen) {
+        // Bloquear el scroll de la página principal cuando el modal está abierto
+        document.body.style.overflow = 'hidden';
+        modalRef.current.focus();
+      } else {
+        // Habilitar el scroll de la página principal cuando el modal se cierra
+        document.body.style.overflow = 'auto';
+      }
+    }, [isOpen]);
+  
+    const handleKeyDown = (event) => {
+      if (event.key === 'Escape') {
+        onClose();
+      }
+    };
+  
+    if (!isOpen) return null;
 
   return (
-    <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 z-50">
-      <div
-      ref={modalRef}
-      tabIndex={-1}
-      onKeyDown={handleKeyDown} 
-      className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8 w-2/3'>
-        <div>
-        <img src={stage1} alt="camiseta1" className="w-full"/>
-        </div>
 
-        <div>
-        <img src={stage1} alt="camiseta1" className="w-full"/>
-        </div> 
-        </div>
-        <button onClick={onClose}>
+        <div className="fixed inset-0 overflow-auto bg-black bg-opacity-75 z-50">
+           <div className='p-10'>
+            <button onClick={onClose} >
                 <svg
                 xmlns="http://www.w3.org/2000/svg"
                 className="h-8 w-8 text-gray-600 dark:text-gray-300 hover:text-gray-800 dark:hover:text-gray-100"
@@ -48,9 +42,26 @@ function Modal({ isOpen, onClose }) {
                     d="M10 19l-7-7m0 0l7-7m-7 7h18"
                 />
                 </svg>
-        </button>
-      
-    </div>
+            </button>  
+            </div> 
+
+            <div className='flex pl-20'>
+                <div
+                ref={modalRef}
+                tabIndex={-1}
+                onKeyDown={handleKeyDown} 
+                className='grid grid-cols-1 sm:grid-cols-4 lg:grid-cols-3 gap-8 w-2/3'>
+
+                    <div>
+                    <img src={stage1} alt="camiseta1" className="w-full"/>
+                    </div>
+
+                    <div>
+                    <img src={stage1} alt="camiseta1" className="w-full"/>
+                    </div> 
+                </div> 
+            </div>
+        </div> 
   );
 }
 
