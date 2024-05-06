18f4d03c457be2cde40509890ffc401c5f351fb0
Abel Ruiz de la Hermosa <abelruiz855@gmail.com>
2024-05-06 15:21:13 +0200

    cambios en title

--- TheBless/src/App.jsx
+++ TheBless/src/App.jsx
@@ -61,7 +61,7 @@ function App() {
         <h1 id="titulo" className="text-center text-6xl p-8">STAGE 1</h1>
         <h1 id="titulo" className="text-center text-4xl">MONEY GOD TEE</h1>
         <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-20 pt-10 pb-10 m-8">
-            <div className="bg-neutral-100 rounded-lg shadow-2xl p-4 hover:scale-105">
+            <div className="bg-neutral-100 rounded-lg shadow-2xl p-4 hover:scale-110">
               <img src="/producto1.png" alt="camiseta1" className="w-full h-auto"/>
               <div className="mt-4">
                 <h3 className="text-center text-lg text-gray-600">MONEY GOD TEE x BLACK</h3>
